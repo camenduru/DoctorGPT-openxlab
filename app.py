e01430b9f29a76db20653b49eb1c34ea971236cb
camenduru <54370274+camenduru@users.noreply.github.com>
2023-08-17 06:51:31 +0300

    test

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import os
 os.chdir(f"/home/xlab-app-center")
 os.system(f"git clone -b v2.1 https://github.com/camenduru/text-generation-webui /home/xlab-app-center/text-generation-webui")
 os.chdir(f"/home/xlab-app-center/text-generation-webui")
-os.system(f"pip install -r requirements.txt")
 
 os.system(f"aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://download.openxlab.org.cn/models/camenduru/medllama2_7b/weight//pytorch_model-00001-of-00002.bin -d /home/xlab-app-center/text-generation-webui/models/medllama2_7b -o pytorch_model-00001-of-00002.bin")
 os.system(f"aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://download.openxlab.org.cn/models/camenduru/medllama2_7b/weight//pytorch_model-00002-of-00002.bin -d /home/xlab-app-center/text-generation-webui/models/medllama2_7b -o pytorch_model-00002-of-00002.bin")
