ef9eee533cfd1f689ba258626ba645d275e959f4
camenduru <54370274+camenduru@users.noreply.github.com>
2023-08-18 13:36:49 +0300

    Add files via upload

---
+++ header.py
@@ -0,0 +1,7 @@
+        with gr.Group():
+            gr.Markdown(f'''
+              🐣 Please follow me for new updates https://twitter.com/camenduru
+              🔥 Please join our discord server https://discord.gg/k5BwmmvJJU
+              🥳 Please join my patreon community https://patreon.com/camenduru
+            ''')
+
