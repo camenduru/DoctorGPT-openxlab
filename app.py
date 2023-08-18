a584b4d548cd7560806af62bafb6231047eca695
camenduru <54370274+camenduru@users.noreply.github.com>
2023-08-18 14:46:59 +0300

    git reset --hard

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 os.chdir(f"/home/xlab-app-center")
 os.system(f"git clone -b v2.1 https://github.com/camenduru/text-generation-webui /home/xlab-app-center/text-generation-webui")
 os.chdir(f"/home/xlab-app-center/text-generation-webui")
+os.system(f"git reset --hard")
 
 os.system(f"aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://download.openxlab.org.cn/models/camenduru/medllama2_7b/weight//pytorch_model-00001-of-00002.bin -d /home/xlab-app-center/text-generation-webui/models/medllama2_7b -o pytorch_model-00001-of-00002.bin")
 os.system(f"aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://download.openxlab.org.cn/models/camenduru/medllama2_7b/weight//pytorch_model-00002-of-00002.bin -d /home/xlab-app-center/text-generation-webui/models/medllama2_7b -o pytorch_model-00002-of-00002.bin")
@@ -21,4 +22,5 @@ os.system(f"sed -i '/def create_ui(default_preset):/a\  with gr.Box(visible=Fals
 os.system(f"sed -i '/def create_ui():/a\  with gr.Box(visible=False):' /home/xlab-app-center/text-generation-webui/modules/ui_session.py")
 os.system(f"sed -i '/def create_ui():/a\  with gr.Box(visible=False):' /home/xlab-app-center/text-generation-webui/modules/training.py")
 os.system(f"sed -i 's/TheEncrypted777/wpp/g' /home/xlab-app-center/text-generation-webui/modules/shared.py")
+
 os.system(f"python server.py --multi-user --model /home/xlab-app-center/text-generation-webui/models/medllama2_7b")
