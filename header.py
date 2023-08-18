6bb6f3c1e300285f2e01eedddec1a22dd695073c
camenduru <54370274+camenduru@users.noreply.github.com>
2023-08-18 14:25:42 +0300

    Update header.py

--- header.py
+++ header.py
@@ -1,10 +1,8 @@
-        with gr.Group():
-            gr.Markdown(f'''
-              <center>
-              ⚠️ <a href="https://github.com/camenduru/DoctorGPT-openxlab#medical-advice-disclaimer">DISCLAIMER: THIS WEBSITE DOES NOT PROVIDE MEDICAL ADVICE!</a><br />
-              🐣 Please follow me for new updates <a href="https://twitter.com/camenduru">https://twitter.com/camenduru</a>
-              🔥 Please join our discord server <a href="https://discord.gg/k5BwmmvJJU">https://discord.gg/k5BwmmvJJU</a>
-              🥳 Please join my patreon community <a href="https://patreon.com/camenduru">https://patreon.com/camenduru</a>
-              </center>
-            ''')
-
+        gr.Markdown(f'''
+          <center>
+          ⚠️ <a href="https://github.com/camenduru/DoctorGPT-openxlab#medical-advice-disclaimer">DISCLAIMER: THIS WEBSITE DOES NOT PROVIDE MEDICAL ADVICE!</a><br />
+          🐣 Please follow me for new updates <a href="https://twitter.com/camenduru">https://twitter.com/camenduru</a>
+          🔥 Please join our discord server <a href="https://discord.gg/k5BwmmvJJU">https://discord.gg/k5BwmmvJJU</a>
+          🥳 Please join my patreon community <a href="https://patreon.com/camenduru">https://patreon.com/camenduru</a>
+          </center>
+        ''')
