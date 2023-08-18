4ee00be1926f71d0c7c7d415e26b2c5e6fb59f7a
camenduru <54370274+camenduru@users.noreply.github.com>
2023-08-18 14:16:18 +0300

    Update header.py

--- header.py
+++ header.py
@@ -2,9 +2,9 @@
             gr.Markdown(f'''
               <center>
               ⚠️ <a href="https://github.com/camenduru/DoctorGPT-openxlab#medical-advice-disclaimer">DISCLAIMER: THIS WEBSITE DOES NOT PROVIDE MEDICAL ADVICE!</a><br />
-              🐣 Please follow me for new updates https://twitter.com/camenduru
-              🔥 Please join our discord server https://discord.gg/k5BwmmvJJU
-              🥳 Please join my patreon community https://patreon.com/camenduru
+              🐣 Please follow me for new updates <a href="https://twitter.com/camenduru">https://twitter.com/camenduru</a>
+              🔥 Please join our discord server <a href="https://discord.gg/k5BwmmvJJU">https://discord.gg/k5BwmmvJJU</a>
+              🥳 Please join my patreon community <a href="https://patreon.com/camenduru">https://patreon.com/camenduru</a>
               </center>
             ''')
 
